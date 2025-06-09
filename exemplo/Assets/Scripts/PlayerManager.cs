de44223bc24d28e873984adf4b34fb178a18f452
NotMayor <soares.saymon@escolar.ifrn.edu.br>
2025-06-09 13:51:52 -0300

    deu tudo errado

--- exemplo/Assets/Scripts/PlayerManager.cs
+++ exemplo/Assets/Scripts/PlayerManager.cs
@@ -1,21 +1,34 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
-   public Animator animator;
-   private MovimentoPlayer movimentoPlayer;
-   public float velocidadeDasAnimacoes = 1;
-   
+    public Animator animator;
+    private MovimentoPlayer movimentoPlayer;
+    public float velocidadeDaAnimacao = 1;
+    
+    private Personagem personagem;
+    
+    
     void Start()
     {
         movimentoPlayer = GetComponent<MovimentoPlayer>();
-        
+        personagem = GetComponent<Personagem>();
     }
-
-   
     void Update()
     {
-        animator.SetBool("Correndo", movimentoPlayer.andando);
-        animator.speed = velocidadeDasAnimacoes;
+        animator.SetBool("Andando", movimentoPlayer.andando);
+        animator.speed = velocidadeDaAnimacao;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Ataque");  
+        }
+
+        if (personagem.Energia() <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        }
+
     }
 }
